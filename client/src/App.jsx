7f91e2a889b03158dd702d186bf8e913ae18a80d
fromagery <ltmpinfo@gmail.com>
2023-01-23 20:46:41 +0300

    package.json relocated to root directory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,31 +12,35 @@ function App() {
           </svg>
           <span className='font-bold text-xl'>noyanbnb</span>            
         </a>
-        {/* Search bar*/}
-        <div className='flex border border-gray-500 items-center rounded-full p-3 gap-5 shadow-md shadow-gray-200'>
+        {/* Search bar
+        add space left and psbly right*/}
+
+        <div className='flex border border-gray-300 items-center rounded-full p-3 pl-5 pr-3 gap-5 shadow-md shadow-gray-200'>
           <div>Anywhere</div>
           <div className="border-[1px] h-7 border-zinc-300 shadow-lg"></div>
           <div>Any week</div>
           <div className="border-[1px] h-7 border-zinc-300 shadow-lg"></div>
           <div>Add guests</div>
-          <button className='bg-red text-white p-3 rounded-full '>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4.5} stroke="currentColor" className="w-3 h-3">
+          {/*Search icon*/}
+          <button className='bg-red text-white p-2 rounded-full '>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3.2} stroke="currentColor" className="w-4 h-4">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
             
           </button>
         </div>
-        {/* User and hamburger */}
-        <div className='flex border border-gray-500 items-center rounded-full p-3 gap-5'>
+        {/* Hamburger and user icons */}
+        <div className='flex border border-gray-300 items-center rounded-full p-3 gap-5'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
           <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-9 h-9">
               <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
           </div>          
         </div>
+        
       </header>
     </div>
   )
