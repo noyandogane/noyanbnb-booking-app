25d2619072f6a3c27b771de3e25837ab90c3de0b
fromagery <ltmpinfo@gmail.com>
2023-01-28 04:36:58 +0300

    fixed 404 error on refresh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom"
-import IndexPage from './pages/IndexPage'
-import LoginPage from './pages/LoginPage'
-import Layout from './Layout'
-import RegisterPage from './pages/RegisterPage'
+import IndexPage from "./pages/IndexPage"
+import LoginPage from "./pages/LoginPage"
+import Layout from "./Layout"
+import RegisterPage from "./pages/RegisterPage"
 
 function App() {  
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        
+      <Route path="/" element={<Layout />}>        
         <Route index element={<IndexPage />}/>  
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/register" element={<RegisterPage />}/>
