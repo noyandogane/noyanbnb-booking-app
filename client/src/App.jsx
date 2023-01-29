9a09d93d43d198ef68932ed2ee4764a9ce7c06c7
fromagery <ltmpinfo@gmail.com>
2023-01-29 14:53:27 +0300

    fix small vercel bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>        
-        <Route path="/*" element={<IndexPage />}/>  
+        <Route index element={<IndexPage />}/>  
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/register" element={<RegisterPage />}/>
       </Route>
