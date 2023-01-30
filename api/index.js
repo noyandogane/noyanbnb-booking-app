a07b0f98b4f2215b50ae09cee3d90ef711549062
fromagery <ltmpinfo@gmail.com>
2023-01-30 04:24:59 +0300

    some api changes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10000);
 
 app.use(express.json());
 app.use(cors({
@@ -9,17 +14,25 @@ app.use(cors({
     origin: 'http://127.0.0.1:5173'
 }));
 
-mongoose.connect("")
+mongoose.connect(process.env.MONGO_URL)
 
 app.get('/test', (req, res) => {
     res.json('Hello World!');
 
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     const {firstName, lastName, email, password} = req.body;
-    res.json({firstName, lastName, email, password});
+    const userDocument = await User.create({
+        firstName,
+        lastName,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt),
+    });
+
+    res.json(userDocument);
 });
+
 
 app.listen(4000, () => {
     
